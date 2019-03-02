1e5fcb9f418a68ee761c2971397f6dfe4b752c18
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-03-02 15:33:34 -0500

    Update Resistors.java

--- src/resistors/Resistors.java
+++ src/resistors/Resistors.java
@@ -1,6 +1,8 @@
 /*
     Matthew Ludwig
-    1 March 2019
+    2 March 2019
+    This program converts a specified resistor colour code, into the corresponding
+    resistance value.
  */
 
 package resistors;
@@ -19,8 +21,14 @@ public class Resistors {
         String inputColour;
         inputColour = JOptionPane.showInputDialog("What is your resistor colour code?" + "\n"
         + "Seperate each with hyphens");
-        String [] colourCode = inputColour.split("-");
+        String [] colourCode = inputColour.split("-"); 
+        String [] resistorValue = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}; 
+        inputColour.toLowerCase();
         
     }
     
 }
+
+// Get the value of the first two resistance colours
+//Set the value of the third to be the first two digits X10^colourindex
+//Print the value of the resistance
