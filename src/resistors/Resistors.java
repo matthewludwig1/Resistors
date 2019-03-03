0061e9bb72508bd811e5ce926e64f665d9ac802a
matthewludwig1 <36999944+matthewludwig1@users.noreply.github.com>
2019-03-03 16:52:45 -0500

    Update Resistors.java

--- src/resistors/Resistors.java
+++ src/resistors/Resistors.java
@@ -1,6 +1,6 @@
 /*
     Matthew Ludwig
-    2 March 2019
+    4 March 2019
     This program converts a specified resistor colour code, into the corresponding
     resistance value.
  */
@@ -20,13 +20,61 @@ public class Resistors {
     public static void main(String[] args) {
         String inputColour;
         inputColour = JOptionPane.showInputDialog("What is your resistor colour code?" + "\n"
-        + "Seperate each with hyphens");
-        String [] colourCode = inputColour.split("-"); 
-        String [] resistorValue = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"}; 
-        inputColour.toLowerCase();
-        
+        + "Seperate each colour with hyphens"); 
+        String [] resistorCode = {"Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", 
+        "Violet", "Grey", "White"};
+         String [] inputText = inputColour.split("-");
+         int resistorValue = inputText.length;  // Will delete this
+         System.out.println(resistorValue);  // Prints out the number of colours
     }
     
+    /**
+     *
+     * @param inputColour
+     * @param resistorCode
+     * @return
+     */
+    public int value (String[] resistorCode) {  //returns the digits of the resistor
+            int resistorValue = 0;
+            for (int i = 0; i < resistorCode.length; i++) {
+                if (resistorCode[i].equalsIgnoreCase("Black")) {
+                    resistorValue += 0;                                // might require resistorValue(i), for the 3 colours                                               
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Brown")) {
+                    resistorValue += 1;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Red")) {
+                    resistorValue += 2;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Orange")) {
+                    resistorValue += 3;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Yellow")) {
+                    resistorValue += 4;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Green")) {
+                    resistorValue += 5;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Blue")) {
+                    resistorValue += 6;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Violet") || resistorCode[i].equalsIgnoreCase
+                ("Purple")) {
+                    resistorValue += 7;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("Grey")) {
+                    resistorValue += 8;
+                }
+                else if (resistorCode[i].equalsIgnoreCase("White")) {
+                    resistorValue += 9;
+                }    
+        }
+    return resistorValue;  
+    }
+    /*
+    public static int totalResistance() {
+        
+    } */
 }
 
 // Get the value of the first two resistance colours
